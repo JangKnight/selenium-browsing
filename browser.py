249a43df2b2c254e9777dfcd04b417d1ae22349c
Professor Knight <profdevent@gmail.com>
2026-08-21 11:38:18 -0400

    chore: initial commit

---
+++ browser.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+import dotenv
+import os
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+dotenv.load_dotenv()
+site_url = os.getenv("SITE")
+opt1 = os.getenv("OPT1")
+
+if not site_url:
+    site_url = input("what site are we testing?")
+
+if opt1:
+    if opt1=="login":
+        username = os.getenv("UNAME")
+        password = os.getenv("PASSW")
+
+        driver.get(f"{site_url}")
+        opt2 = os.getenv("OPT2")
+        if not opt2:
+            opt2 = input("what is the id of the login section?")
+
+        driver.implicitly_wait(10)
+        login_element = driver.find_element(By.ID, f"{opt2}")
+        #login_element = driver.find_element(By.CLASS_NAME, f"{opt2}")
+        #print(login_element.get_attribute("outerHTML"))
+
+        if not username:
+            username = input("enter username")
+        if not password:
+            password = input("enter password")
+
+        driver.implicitly_wait(10)
+        login_element.find_element(By.NAME, "username").send_keys(username)
+
+        login_element.find_element(By.NAME, "password").send_keys(password)
+        login_element.find_element(By.NAME, "login").click()
+        driver.implicitly_wait(10)
+
+        home_page = driver.find_element(By.TAG_NAME, "body")
+        #print(home_page.get_attribute("innerHTML"))
+
+        # TESTED USING WECHALL's SITE
+
+        challenges_tab = home_page.find_element(By.CSS_SELECTOR, '#wc_menu a[href="/challs"]')
+        #print(challenges_tab.get_attribute("outerHTML"))
+        challenges_tab.click()
+
+        chall_page = driver.find_element(By.TAG_NAME, "body")
+        desired_chall = chall_page.find_element(By.CSS_SELECTOR, '#page .wc_chall_table a[href="/challenge/training/programming1/index.php"]')
+        #print(desired_chall.get_attribute("outerHTML"))
+        desired_chall.click()
+
+        driver.find_element(By.CSS_SELECTOR, '#page .box .box_c a[href="index.php?action=request"]').click()
+        msg_page = driver.find_element(By.TAG_NAME, "body").get_attribute("outerHTML")
+        msg = driver.find_element(By.TAG_NAME, "body").get_attribute("innerHTML")
+
+        print("\nCookies\n------------------")
+        ctf_session = requests.Session()
+        for cookie in driver.get_cookies():
+            ctf_session.cookies.set(cookie['name'], cookie['value'])
+            print(cookie)
+        print("------------------")
+
+        res = ctf_session.post(f"http://www.wechall.net/challenge/training/programming1/index.php?answer={msg}")
+        print(res.text)
+
+
+
+
+
+
+done = False
+while not done:
+    done_eval = input("type 'yes' when you are done using this browser: ")
+    if done_eval == 'yes' or done_eval == 1 or done_eval == 'y':
+        done = True
+
+driver.quit()
+
+
